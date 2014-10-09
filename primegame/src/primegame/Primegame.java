244ba1b0055cf45bd2a34bc5ff11d2c6cd125cbb
stephenjohnwrightfarrington <stephenjohnwrightfarrington@client-75-102-111-116.mobility-up.psu.edu>
2014-10-09 13:11:42 -0400

    added hurr durr comment to l13

---
+++ primegame/src/primegame/Primegame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primegame;
+
+/**
+ *
+ * @author stephenjohnwrightfarrington
+ */
+public class Primegame
+{
+    //HURR DURR
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
